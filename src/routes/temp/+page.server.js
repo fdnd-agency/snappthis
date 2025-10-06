b53629b8bbe38a9663aa702be90f5c44557c453c
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-10-06 16:31:18 +0200

    feat: moved files from temp to [snappid]

--- src/routes/temp/+page.server.js
+++
@@ -1,4 +0,0 @@
-export async function load({ url }) {
-    const snappDetails = await fetch('https://fdnd-agency.directus.app/items/snappthis_snap?fields=*,author.name').then(response => response.json())
-    return { snappDetails: snappDetails.data }
-}
