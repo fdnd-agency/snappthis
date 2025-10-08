81a18456faec8ee7c6895ef1932647a1436220c4
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-10-08 08:44:09 +0200

    replacing import data
    Replace the loading data into the page itself
    
    #19

--- src/routes/group-overview/+page.svelte
+++ src/routes/group-overview/+page.svelte
@@ -1,12 +1,30 @@
 <script>
     import Header from '$lib/components/header.svelte'
     import Dropdown from '$lib/components/dropdown-button.svelte'
+
+    import { onMount } from 'svelte'
+    import { loadPictures } from '$lib/components/index.js'
+
+    let pictures = [];
+
+    onMount(async () => {
+        pictures = await loadPictures()
+    });
+
 </script>
 
 <Header />
 
 <main>
     <Dropdown />
+
+    {#each pictures as picture}
+        <picture>
+            <source srcset={`https://fdnd-agency.directus.app/assets/${picture.picture}`} type="image/webp" media="(min-width: 512px)">
+            <source srcset="/example/example-photo.webp" type="image/webp" media="(min-width: 512px)">
+            <img class="pic" src={`https://fdnd-agency.directus.app/assets/${picture.picture}`} height="512" width="512" alt="example photo"/>
+        </picture>
+    {/each}
 </main>
 
 <style>
