4fcc5352fa848033267fa8f5fd1604167c29097b
Iris van Willigen <iris.van.willigen@gmail.com>
2025-10-15 11:09:25 +0200

    Feat: add basic HTML and styling #69

--- src/routes/group-overview/+page.svelte
+++ src/routes/group-overview/+page.svelte
@@ -1,1 +1,44 @@
-<h1>Group Overview</h1>
+<script>
+    let { data } = $props()
+    const groups = data.groups
+</script>
+
+<header>
+    <h1>Group Overview</h1>
+    <a href="/">Add group</a>
+</header>
+
+<main>
+    <ul>
+        {#each groups as group}
+            <li>
+                <p>{group.name}</p>
+                <p>{group.users.length}</p>
+                <a href="/">Add person</a>
+                <a href="/">Add SnappMap</a>
+            </li>
+        {/each}
+    </ul>
+</main>
+
+<style>
+    header {
+        background-color: #172f3b;
+        background-color: var(--primary-color);
+        width: 100%;
+        display: flex;
+        color: white;
+        font-family: 'Bariol';
+    }
+
+    ul,
+    li {
+        margin-inline: 0;
+        padding-inline: 0;
+    }
+    li {
+        list-style: none;
+        padding: 1em;
+        border-bottom: 1px solid grey;
+    }
+</style>
