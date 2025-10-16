65f4d36a126d23df5a2a050e62e93608833cea1a
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-10-16 12:06:04 +0200

    Feat: server.js + name and tags user #75

---
+++ src/routes/you/+page.server.js
@@ -0,0 +1,15 @@
+// export async function load({ url, params }) {
+//     const user = await fetch(
+//         `https://fdnd-agency.directus.app/items/snappthis_user/${params.userid}`
+//     ).then((response) => response.json())
+//     return { user: user.data }
+// }
+
+export async function load({ url }) {
+    const user = await fetch(
+        `https://fdnd-agency.directus.app/items/snappthis_user/902bf505-c020-4663-aee8-2cef8074b8f2`
+    ).then((response) => response.json())
+    return { user: user.data }
+}
+
+
--- src/routes/you/+page.svelte
+++ src/routes/you/+page.svelte
@@ -1,1 +1,15 @@
+<script>
+    let { data } = $props()
+    const user = data.user
+</script>
+
 <h1>You</h1>
+
+<h2>{ user.name }</h2>
+
+<ul>
+    <li>{ user.gender }</li>
+    <li>{ user.birthdate.substring(0, 10) }</li>
+    <li>@{ user.usecase }</li>
+    <li>{ user.email }</li>
+</ul>
