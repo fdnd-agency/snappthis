2d294a736c0fb94b7cdbe79a24e3cfdfd1b77723
Iris van Willigen <iris.van.willigen@gmail.com>
2025-10-17 09:42:58 +0200

    Feat: add 'back' and 'plus' icon as svg and components #69

--- src/lib/components/icons/LogoIcon.svelte
+++ src/lib/components/icons/LogoIcon.svelte
@@ -1,9 +1,14 @@
+<script>
+    let { title } = $props()
+</script>
+
 <svg
     width="47"
     height="48"
     viewBox="0 0 47 48"
     fill="none"
     xmlns="http://www.w3.org/2000/svg">
+    <title id="svgTitle">{title ? title : ""}</title>
     <path
         fill-rule="evenodd"
         clip-rule="evenodd"
