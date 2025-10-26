4cf014b477adbc06a3b6b1c5ac03f831a31d8c7f
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-10-26 09:25:14 +0100

    19-fix-layout-grid
    Put the info into one but hide it with display: none
    
    #19

--- src/routes/[snappmapid]/+page.svelte
+++ src/routes/[snappmapid]/+page.svelte
@@ -1,116 +1,124 @@
 <script>
-    let { data } = $props()
-    const snaps = data.snaps[0].snaps
-    const id = data.id
+    let { data } = $props();
+    const snaps = data.snaps[0].snaps;
+    const id = data.id;
 
-    import Header from '$lib/components/Header.svelte'
-    import Star from '$lib/components/icons/Star.svelte'
-    import Tomato from '$lib/components/icons/Tomato.svelte'
-    import Heart from '$lib/components/icons/Heart.svelte'
+    import Header from "$lib/components/Header.svelte";
+    import Star from "$lib/components/icons/Star.svelte";
+    import Tomato from "$lib/components/icons/Tomato.svelte";
+    import Heart from "$lib/components/icons/Heart.svelte";
 
-    import GridOne from '$lib/components/icons/Grid1.svelte'
-    import GridTwo from '$lib/components/icons/Grid2.svelte'
-    import GridThree from '$lib/components/icons/Grid3.svelte'
-    import GridFour from '$lib/components/icons/Grid4.svelte'
-    import GridFive from '$lib/components/icons/Grid5.svelte'
-    import ListView from '$lib/components/icons/Listview.svelte'
+    import GridOne from "$lib/components/icons/Grid1.svelte";
+    import GridTwo from "$lib/components/icons/Grid2.svelte";
+    import GridThree from "$lib/components/icons/Grid3.svelte";
+    import GridFour from "$lib/components/icons/Grid4.svelte";
+    import GridFive from "$lib/components/icons/Grid5.svelte";
+    import ListView from "$lib/components/icons/Listview.svelte";
 
-
-    let gridsize = $state('grid1')
-
+    let gridsize = $state("grid1");
 </script>
 
-<Header {data}/>
+<Header {data} />
 
-            <label>
-                <input type="radio" name="settings" value="grid1" bind:group={gridsize} />
-                <GridOne />
-            </label>
-            <label>
-                <input type="radio" name="settings" value="grid2" bind:group={gridsize} />
-                <GridTwo />
-            </label>
-            <label>
-                <input type="radio" name="settings" value="grid3" bind:group={gridsize} />
-                <GridThree />
-            </label>
-            <label>
-                <input type="radio" name="settings" value="grid4" bind:group={gridsize} />
-                <GridFour />
-            </label>
-            <label>
-                <input type="radio" name="settings" value="grid5" bind:group={gridsize} />
-                <GridFive />
-            </label>
-            <label>
-                <input type="radio" name="settings" value="list-view" bind:group={gridsize} />
-                <ListView />
-            </label>
+<label>
+    <input type="radio" name="settings" value="grid1" bind:group={gridsize} />
+    <GridOne />
+</label>
+<label>
+    <input type="radio" name="settings" value="grid2" bind:group={gridsize} />
+    <GridTwo />
+</label>
+<label>
+    <input type="radio" name="settings" value="grid3" bind:group={gridsize} />
+    <GridThree />
+</label>
+<label>
+    <input type="radio" name="settings" value="grid4" bind:group={gridsize} />
+    <GridFour />
+</label>
+<label>
+    <input type="radio" name="settings" value="grid5" bind:group={gridsize} />
+    <GridFive />
+</label>
+<label>
+    <input
+        type="radio"
+        name="settings"
+        value="list"
+        bind:group={gridsize}
+    />
+    <ListView />
+</label>
 
-
-<div class='snaps-grid-{gridsize}'>
+<div class="snaps-{gridsize}">
     {#each snaps as snap}
         <picture>
-            <a href="/{ snap.uuid }/{ snap.picture}">
-                    <img src={"https://fdnd-agency.directus.app/assets/" + snap.picture} alt="" />
+            <a href="/{snap.uuid}/{snap.picture}">
+                <img
+                    src={"https://fdnd-agency.directus.app/assets/" +
+                        snap.picture}
+                    alt=""
+                />
             </a>
         </picture>
-    {/each}
-</div>
-
-<div class="snaps-list-">
-    {#each snaps as snap}
-    <p> {snap.author} </p>
-    <p> {snap.location} </p>
-        <picture>
-            <a href="/{ snap.uuid }/{ snap.picture}">
-                    <img src={"https://fdnd-agency.directus.app/assets/" + snap.picture} alt="" />
-            </a>
-        </picture>
-    <button aria-label="Star"> <Star /> </button>
-    <button aria-label="Tomato"> <Tomato /> </button>
-    <button aria-label="Heart"> <Heart /> </button>
+        <div class="list-container">
+            <p>{snap.author}</p>
+            <p>{snap.location}</p>
+            <button aria-label="Star"> <Star /> </button>
+            <button aria-label="Tomato"> <Tomato /> </button>
+            <button aria-label="Heart"> <Heart /> </button>
+        </div>
     {/each}
 </div>
 
 <style>
-.container {
+    .container {
         display: grid;
         gap: 1em;
         top: 0;
         list-style: none;
         margin: 1em;
-}
+    }
 
-picture img {
-    border-radius: var(--border-radius-desktop);
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-}
+    picture img {
+        border-radius: var(--border-radius-desktop);
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+    }
 
-.snaps-grid-grid1 { 
-    display: grid;
-    grid-template-columns: repeat(1, 1fr); 
-}
+    .snaps-grid1 {
+        display: grid;
+        grid-template-columns: repeat(1, 1fr);
+    }
 
-.snaps-grid-grid2 { 
-    display: grid;
-    grid-template-columns: repeat(2, 1fr); 
-}
+    .snaps-grid2 {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+    }
 
-.snaps-grid-grid3 { 
-    display: grid;
-    grid-template-columns: repeat(3, 1fr); 
-}
+    .snaps-grid3 {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
 
-.snaps-grid-grid4 { 
-    display: grid;
-    grid-template-columns: repeat(4, 1fr); 
-}
+    .snaps-grid4 {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+    }
 
-.snaps-grid-grid5 { 
-    display: grid;
-    grid-template-columns: repeat(5, 1fr); 
-}
+    .snaps-grid5 {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+    }
+
+    .list-container {
+
+    }
+
+    .snaps-list {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+    }
+
 </style>
