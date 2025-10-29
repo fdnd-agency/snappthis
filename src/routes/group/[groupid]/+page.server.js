5f00cf3b477533e76b73852c940a59813ed80d01
Iris van Willigen <iris.van.willigen@gmail.com>
2025-10-29 21:32:12 +0100

    Feat: move snappmapid slug route into snappmap folder #95

--- src/routes/group/[groupid]/+page.server.js
+++ src/routes/group/[groupid]/+page.server.js
@@ -5,5 +5,5 @@ export async function load({ url, params }) {
         `https://fdnd-agency.directus.app/items/snappthis_group/?fields=snappmap.*.*&filter[snappmap][snappthis_group_uuid][uuid][_eq]=${params.groupid}&sort=-snappmap.snappthis_snapmap_uuid.time_start&limit=1`
     ).then((response) => response.json())
     const latestUUID = latest.data[0].snappmap[0].snappthis_snapmap_uuid.uuid
-    redirect(308, `${url.pathname}/${latestUUID}`)
+    redirect(308, `${url.pathname}/snappmap/${latestUUID}`)
 }
