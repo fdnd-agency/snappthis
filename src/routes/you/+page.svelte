693e353832ce9b4744b0fdc9d6a5b46c06e2504f
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-10-29 12:49:35 +0100

    Feat: prettier

--- src/routes/you/+page.svelte
+++ src/routes/you/+page.svelte
@@ -8,50 +8,52 @@
 </script>
 
 <section class="user-info">
-    <h1>{ user.name }</h1>
+    <h1>{user.name}</h1>
 
     <ul class="your-info">
-        <li>{ user.gender }</li>
-        <li>{ user.birthdate.substring(0, 10) }</li>
-        <li>@{ user.usecase }</li>
-        <li><strong>{ user.email }</strong></li>
+        <li>{user.gender}</li>
+        <li>{user.birthdate.substring(0, 10)}</li>
+        <li>@{user.usecase}</li>
+        <li><strong>{user.email}</strong></li>
     </ul>
 
     <ul class="your-data">
-        <li> <GalleryIcon></GalleryIcon> 54 snapps</li>
-        <li> <a href="/group-overview"> <GroupIcon></GroupIcon> { user.groups } groups </a></li>
-        <li > <StarIcon></StarIcon> 21 stars</li>
+        <li><GalleryIcon></GalleryIcon> 54 snapps</li>
+        <li>
+            <a href="/group-overview">
+                <GroupIcon></GroupIcon>
+                {user.groups} groups
+            </a>
+        </li>
+        <li><StarIcon></StarIcon> 21 stars</li>
     </ul>
 </section>
 
-
 <style>
+    h1,
+    li {
+        font-family: 'bariol';
+    }
 
-h1, li {
-    font-family: 'bariol';
-}
+    a {
+        color: var(--neutral-color-0);
+        text-decoration: none;
+    }
 
-a {
-    color: var(--neutral-color-0);
-    text-decoration: none;
-}
+    .user-info {
+        background-color: var(--neutral-color-90);
+        margin: 0;
+        padding: 2em;
+        height: 45vh;
+        place-items: center;
+    }
 
-.user-info {
-    background-color: var(--neutral-color-90);
-    margin: 0;
-    padding: 2em;
-    height: 45vh;   
-    place-items: center;
-
-}
-
-ul {
-    display: flex;
-    flex-wrap: wrap;
-    gap: .5em;
-    list-style: none;
-    padding: 0;
-    justify-content: center;
-}
-
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.5em;
+        list-style: none;
+        padding: 0;
+        justify-content: center;
+    }
 </style>
