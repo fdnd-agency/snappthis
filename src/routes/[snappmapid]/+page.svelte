4cef3d6b7a1897896f4ac2ebd060eaf70506b696
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-10-29 09:39:53 +0100

    fix: formating + GridOne state
    added a grid one state
    
    #96

--- src/routes/[snappmapid]/+page.svelte
+++ src/routes/[snappmapid]/+page.svelte
@@ -2,25 +2,27 @@
     let { data } = $props()
     const snaps = data.snaps[0].snaps
     const id = data.id
+    let gridsize = $state('grid1')
 
-    const snapMap = data.snaps?.[0]   
+    const snapMap = data.snaps?.[0]
     import Header from '$lib/components/Header.svelte'
     import Star from '$lib/components/icons/StarIcon.svelte'
     import Tomato from '$lib/components/icons/TomatoIcon.svelte'
     import Heart from '$lib/components/icons/HeartIcon.svelte'
+    import GridOne from '$lib/components/icons/Grid1Icon.svelte'
     import GridTwo from '$lib/components/icons/Grid2Icon.svelte'
     import GridThree from '$lib/components/icons/Grid3Icon.svelte'
     import GridFour from '$lib/components/icons/Grid4Icon.svelte'
     import GridFive from '$lib/components/icons/Grid5Icon.svelte'
     import ListView from '$lib/components/icons/ListviewIcon.svelte'
-    let gridsize = $state('grid1')
+
 </script>
 
-<Header title="{snapMap.name}" />
+<Header title={snapMap.name} />
 
 <label>
     <input type="radio" name="settings" value="grid1" bind:group={gridsize} />
-    <GridTwo />
+    <GridOne />
 </label>
 <label>
     <input type="radio" name="settings" value="grid2" bind:group={gridsize} />
@@ -87,49 +89,60 @@
         margin: 0;
         padding: 0;
     }
+
     picture img {
         border-radius: 5%;
         width: 100%;
         height: auto;
         object-fit: cover;
     }
+
     .snaps-grid1 {
         display: grid;
         grid-template-columns: repeat(1, 1fr);
     }
+
     .snaps-grid2 {
         display: grid;
         grid-template-columns: repeat(2, 1fr);
     }
+
     .snaps-grid3 {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
     }
+
     .snaps-grid4 {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
     }
+
     .snaps-grid5 {
         display: grid;
         grid-template-columns: repeat(5, 1fr);
     }
+
     .list-container {
         display: grid;
         grid-template-rows: 50% 40% 10%;
     }
+
     .feedback {
         display: flex;
         justify-content: flex-end;
         height: 1em;
     }
+
     .snaps-list {
         display: grid;
         grid-template-columns: 30% 70%;
         padding: 1em;
     }
+
     .list-container {
         display: none;
     }
+
     .list-container.visible {
         display: block;
     }
