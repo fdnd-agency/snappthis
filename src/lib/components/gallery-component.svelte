a42fbcb9350ed679c2e4fc5ee126a37bfd6f46c9
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-11-12 11:50:15 +0100

    feat-setup-gallery-component
    The idea is that the component that will be created includes the layout navigation, all the data in the component is fallback if content not loaded, content will be loaded on the page itself.
    
    #98

---
+++ src/lib/components/gallery-component.svelte
@@ -0,0 +1,55 @@
+<script>
+
+</script>
+
+<div class="layout-navigation">
+    <raio>
+        <input>
+        <!-- Grid state 2 -->
+    </raio>
+
+    <raio>
+        <input>
+        <!-- Grid state 3 -->
+    </raio>
+
+    <raio>
+        <input>
+        <!-- Grid state 4 -->
+    </raio>
+
+    <raio>
+        <input>
+        <!-- Grid state 5 -->
+    </raio>
+
+    <raio>
+        <input>
+        <!-- Listview -->
+    </raio>
+</div>
+
+<div class="snaps">
+    <!-- for each loop -->
+     <!-- Data wordt ingeladen op de pagina zelf -->
+      <!-- geef dat aan op de pagina gebruik voor nu een fallback -->
+       <li>
+    <picture>
+        <!-- WEBP -->
+        <source 
+        srcset=""> 
+
+        <!-- AVIF -->
+        <source 
+        srcset=""> 
+
+        <!-- normale formaat -->
+        <img>
+    </picture>
+            </li>
+
+            <!-- name author -->
+             <!-- location of snap -->
+              <!--  -->
+</div>
+
