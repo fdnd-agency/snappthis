b93c8531dac135bc270ab6fca102d3b803feb592
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-11-14 11:26:59 +0100

    feat-button-component
    button-example
    
    #20

---
+++ src/lib/components/snappmap-overview/XL-button.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    import GridTwo from '$lib/assets/icons/Grid2Icon.svelte'
+
+</script>
+
+<button>
+    <GridTwo />
+    <label>XL</label>
+    <label>Xlarge</label>
+</button>
+
+<style>
+    button {
+        background-color: var(--neutral-color-90);
+        color: var(--neutral-color-0);
+    }
+</style>
