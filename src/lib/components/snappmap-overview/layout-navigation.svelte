b1fe41c5cda8a14bcc7955cd2744370e95fa5366
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-11-17 10:15:18 +0000

    feat: labels-responsive
    labels-should be responsive from 720px it shows the whole value
    
    #20

--- src/lib/components/snappmap-overview/layout-navigation.svelte
+++ src/lib/components/snappmap-overview/layout-navigation.svelte
@@ -4,16 +4,35 @@
     import GridFour from '$lib/components/icons/Grid4Icon.svelte'
     import GridFive from '$lib/components/icons/Grid5Icon.svelte'
     import ListView from '$lib/components/icons/Grid5Icon.svelte'
+
+    import { onMount } from 'svelte'
 </script>
 
-<button> </button>
+<button class="dropdownmenu-button"> 
+    <GridTwo/> 
+    <!-- there should be a state here where it shows the current state -->
+</button>
 
-<div class="dropdownmenu-mobile">
-    <span> <GridTwo></GridTwo> <label>XL</label> <label>XLarge</label> </span>
-    <span> <GridThree></GridThree> <label>L</label> <label>Large</label> </span>
-    <span> <GridFour></GridFour> <label>M</label> <label>Medium</label> </span>
+<div class="dropdownmenu">
+    <span> 
+        <GridTwo></GridTwo> 
+        <label>XL</label> 
+        <label>XLarge</label> 
+    </span>
+    <span> 
+        <GridThree></GridThree> 
+        <label>L</label> 
+        <label>Large</label> 
+    </span>
+    <span> 
+        <GridFour></GridFour>
+         <label>M</label> 
+         <label>Medium</label> 
+    </span>
     <span>
-        <ListView></ListView> <label>List</label> <label>List View</label>
+        <ListView></ListView> 
+        <label>List</label> 
+        <label>List View</label>
     </span>
 </div>
 
@@ -44,6 +63,24 @@
 </div>
 
 <style>
+    .dropdownmenu {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+        background-color: var(--neutral-color-80);
+
+        span label:nth-of-type(1) {
+            @media screen and (min-width: 720px){
+                display: none;
+            }
+        }
+
+        span label:ntxh-of-type(2) {
+            @media screen and (max-width: 720px){
+                display: none;
+            }
+        }
+    }
 
 
 </style>
