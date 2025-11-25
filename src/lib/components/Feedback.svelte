0f33eb2027975620a18f2b1150c4ea8aba46c35f
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-11-25 13:59:31 +0100

    build: layout-feedback
    added more worked on components

---
+++ src/lib/components/Feedback.svelte
@@ -0,0 +1,145 @@
+<script>
+    import { onMount } from 'svelte'
+
+    import Heart from '$lib/components/icons/HeartIcon.svelte'
+    import Star from '$lib/components/icons/StarIcon.svelte'
+    import Tomato from '$lib/components/icons/TomatoIcon.svelte'
+
+    onMount(async () => {
+        const heartsAmount = document.querySelector('.feedback-heart');
+        const starAmount = document.querySelector('.feedback-star');
+        const tomatoAmount = document.querySelector('.feedback-tomato');
+
+        const buttonTomato = document.querySelector('.tomato-button');
+        const buttonStar = document.querySelector('.star-button');
+        const buttonHeart = document.querySelector('.heart-button');
+
+        let heartCount = 0; 
+        let tomatoCount = 0;
+        let starCount = 0;
+
+        const fillHeart = document.querySelector('.heart-icon')
+        const fillTomato = document.querySelector('.tomato-icon')
+        const fillStar = document.querySelector('.star-icon')
+
+            // Limit heart to max 1
+            buttonHeart.addEventListener('click', () => {
+            fillHeart.style.color = "green";
+            buttonHeart.style.backgroundColor = "#6BB8DE";
+
+            if (heartCount < 1) {
+                heartCount = 1;
+                heartsAmount.textContent = heartCount;
+            }
+            });
+
+            // Tomato increments normally
+            buttonTomato.addEventListener('click', () => {
+                fillTomato.style.color = "red";
+                buttonTomato.style.backgroundColor = "#6BB8DE";
+
+            if (tomatoCount < 1) {
+                tomatoCount = 1;
+                tomatoAmount.textContent = tomatoCount;
+            }
+            });
+
+            // Star increments normally
+            buttonStar.addEventListener('click', () => {
+                fillStar.style.color = "yellow";
+                buttonStar.style.backgroundColor = "#6BB8DE";
+            if (starCount < 1) {
+                starCount = 1;
+                starAmount.textContent = starCount;
+            }
+            });
+    })
+
+</script>
+
+<div class="feedback-container">
+<form class="feedback">
+    <button class="heart-button">
+        <label>
+            <div class="heart-icon">
+                <Heart />
+            </div>
+            <p class="feedback-heart"> 0 </p>
+        </label>
+    </button>
+
+    <button class="star-button">
+        <label>
+            <div class="star-icon">
+            <Star />
+            </div>
+            <p class="feedback-star"> 0 </p>
+        </label>
+    </button>
+
+    <button class="tomato-button">
+        <label>
+            <div class="tomato-icon">
+            <Tomato />
+            </div>
+            <p class="feedback-tomato"> 0 </p>
+        </label>
+    </button>
+</form>
+</div>
+
+<style>
+    .feedback-container {
+        container-type: inline-size;
+    }
+
+    .feedback button {
+        background-color: var(--neutral-color);
+        color: var(--neutral-color-40);
+        height: 3.5em;
+        width: 3em;
+        border-radius: 8px;
+        font: 'Bariol';
+        line-height: 0;
+    }
+
+    .tomato-icon, .heart-icon, .star-icon {
+        display: flex;
+        justify-content: center;
+        position: absolute;
+        
+    }
+
+    .feedback-tomato, .feedback-heart, .feedback-star {
+        position: relative;
+        top: 0.5em;
+
+        font-size: 18px;
+    }
+
+
+@container (min-width: 720px) {
+    .feedback button {
+        background-color: var(--neutral-color);
+        color: var(--neutral-color-40);
+        height: 4em;
+        width: 4em;
+        border-radius: 8px;
+        font: 'Bariol';
+    }
+
+    .feedback button label {
+        display: flex;
+        justify-content: center;
+        gap: 1em;
+
+
+    }
+
+    .tomato-icon, .heart-icon, .star-icon {
+        display: flex;
+        align-items: center;
+        scale: 1.5;
+    }
+}
+</style>
---
+++ src/lib/components/Layout-navigation.svelte
@@ -0,0 +1,132 @@
+<script>
+    import GridTwo from '$lib/components/icons/Grid2Icon.svelte'
+    import GridThree from '$lib/components/icons/Grid3Icon.svelte'
+    import GridFour from '$lib/components/icons/Grid4Icon.svelte'
+    import GridFive from '$lib/components/icons/Grid5Icon.svelte'
+    import ListView from '$lib/components/icons/Grid5Icon.svelte'
+
+    import { onMount } from 'svelte'
+
+    let selected = "xlarge";
+
+    function selectOption(value) {
+        selected = value;
+    }
+</script>
+
+<div class="dropdownmenu">
+    <span class="span-content"
+        class:selected={selected === "xlarge"}
+        on:click={() => selectOption("xlarge")}
+    >
+        <input type="radio" name="layout" value="xlarge" class="radio-element">  
+        <GridTwo></GridTwo> 
+        <label for="xlarge">XL</label> 
+        <label for="xlarge">XLarge</label> 
+    </span>
+
+    <span class="span-content"
+    class:selected={selected === "large"}
+        on:click={() => selectOption("large")}
+    >
+        <input type="radio" name="layout" value="large" class="radio-element"> 
+        <GridThree></GridThree> 
+        <label for="large">L</label> 
+        <label for="large">Large</label> 
+    </span>
+
+    <span class="span-content"
+        class:selected={selected === "medium"}
+        on:click={() => selectOption("medium")}
+    >
+        <input type="radio" name="layout" value="medium" class="radio-element"> 
+        <GridFour></GridFour>
+         <label for="medium">M</label> 
+         <label for="medium">Medium</label> 
+    </span>
+
+    <span class="span-content"
+        class:selected={selected === "small"}
+        on:click={() => selectOption("small")}
+    >
+        <input type="radio" name="layout" value="small" class="radio-element"> 
+        <GridFive></GridFive>
+         <label for="small">S</label> 
+         <label for="small">Small</label> 
+    </span>
+
+    <span class="span-content"
+        class:selected={selected === "list-view"}
+        on:click={() => selectOption("list-view")}
+    >
+        <input type="radio" name="layout" value="list" class="radio-element"> 
+        <ListView></ListView> 
+        <label for="list">List</label> 
+        <label for="list">List View</label>
+    </span>
+</div>
+
+
+<style>
+    .dropdownmenu {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+        background-color: var(--neutral-color-80);
+        padding: 1em;
+        width: fit-content;
+
+        @media (min-width: 1080px) {
+            width: 100%;
+
+        }
+    
+        span:nth-of-type(4) {
+            @media screen and (max-width: 1080px){
+                display: none;
+            }
+        }
+
+        span:nth-of-type(3) {
+            @media screen and (max-width: 720px){
+                display: none;
+            }
+        }
+
+        .span-content {
+            display: flex;
+            align-items: center;
+            justify-content: space-evenly;
+            background-color: var(--neutral-color-90);
+            padding: 1em;
+            border-radius: 16px;
+        }
+
+        span label {
+            align-items: center;
+            flex-direction: column;
+            padding-left: 3em;
+        }
+
+        span label:nth-of-type(1) {
+            @media screen and (min-width: 720px){
+                display: none;
+            }
+        }
+
+        span label:nth-of-type(2) {
+            @media screen and (max-width: 720px){
+                display: none;
+            }
+        }
+    }
+
+    .radio-element {
+        display: none;
+    }
+
+.span-content.selected {
+    background-color: var(--primary-color-60);
+}
+</style>
+
