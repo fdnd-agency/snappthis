d5e8100b227adde786f1f4fd30609afaf264e3c0
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-11-25 14:35:00 +0100

    build: re-routing
    Rerouting the group/snapmap when you open a group it will load the group with all snapmaps
    
    #95

--- src/routes/group/[groupid]/+page.svelte
+++ src/routes/group/[groupid]/+page.svelte
@@ -1,4 +1,21 @@
-<h1>
-    Non-existent overview page of all the SnappMaps belonging to a certain
-    group.
-</h1>
+<script>
+    import Header from '$lib/components/Header.svelte'
+    import ArrowLeftIcon from '$lib/components/icons/ArrowLeftIcon.svelte'
+    import LogoIcon from '$lib/components/icons/LogoIcon.svelte'
+    import PlusIcon from '$lib/components/icons/PlusIcon.svelte'
+    import UserIcon from '$lib/components/icons/UserIcon.svelte'
+
+    let { data } = $props()
+    const groups = data.groups
+</script>
+<Header title="{groups.name}"/>
+
+    {#each snapmaps as snapmap}
+
+    {/each}
+
+
+
+
+
+
--- src/routes/group/[groupid]/[snappmapid]/+page.server.js
+++ src/routes/group/[groupid]/[snappmapid]/+page.server.js
@@ -1,17 +1,29 @@
 export async function load({ url, params }) {
-    const snapsRes = await fetch(
-        `https://fdnd-agency.directus.app/items/snappthis_snapmap/?fields=snaps.*,name,uuid&filter[uuid][_eq]=${params.snappmapid}`
-    )
-    const snapsData = await snapsRes.json()
+  const snapsRes = await fetch(
+    `https://fdnd-agency.directus.app/items/snappthis_snapmap/?fields=snaps.*,name,uuid&filter[uuid][_eq]=${params.snappmapid}`
+  );
+  const snapsData = await snapsRes.json();
 
-    const snappMapsRes = await fetch(
-        'https://fdnd-agency.directus.app/items/snappthis_snapmap?fields=name,uuid'
-    )
-    const snappMapsData = await snappMapsRes.json()
+  const usersRes = await fetch(
+    'https://fdnd-agency.directus.app/items/snappthis_user?fields=name,uuid'
+  );
+  const usersData = await usersRes.json();
 
-    return {
-        snaps: snapsData.data,
-        snapName: snappMapsData.data,
-        id: params.snappmapid,
-    }
+  const snappMapsRes = await fetch(
+    'https://fdnd-agency.directus.app/items/snappthis_snapmap?fields=name,uuid'
+  )
+  const snappMapsData = await snappMapsRes.json()
+
+  return {
+    snaps: snapsData.data,
+    users: usersData.data,
+    snapmap: snappMapsData.data, 
+    id: params.snappmapid,
+  };
+}
+
+export async function load({ url, params }) {
+    let latest = await fetch(
+        `https://fdnd-agency.directus.app/items/snappthis_group/?fields=snappmap.*.*&filter[snappmap][snappthis_group_uuid][uuid][_eq]=${params.groupid}`
+    ).then((response) => response.json())
 }
