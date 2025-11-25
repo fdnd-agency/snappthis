6c1a6b194089da0e8ce43031139e4adf15ad4ba0
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-11-25 14:02:21 +0100

    fix: route
    the route was incorrect
    
    #98

---
+++ src/routes/group/[groupid]/[snappmapid]/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+    import Header from '$lib/components/Header.svelte'
+    import Gallery from '$lib/components/snappmap-overviewpage/gallery-component.svelte'
+    import Card from '$lib/components/snappmap-overviewpage/card.svelte'
+
+    // icons
+
+    let { data } = $props()
+    const snaps = data.snaps[0].snaps
+    const id = data.id
+    const users = data.users
+    const snapmap = data.snaps?.[0]
+
+
+    const userMap = new Map(users.map((u) => [u.uuid, u.name]))
+</script>
+
+<svelte:head>
+    <title>{snapmap.name}</title>
+</svelte:head>
+
+<Header title={snapmap.name}></Header>
+
+<nav>
+    <!-- title of group (component) -->
+    <!-- sort function (component) -->
+</nav>
+
+<main>
+{#each snaps as snap}
+    <Gallery
+        url={snapmap.uuid}
+        snap={snap.uuid}
+        picture={snap.picture}
+        author={snap.author}
+        location={snap.location}
+    />
+{/each}
+</main>
+
+<style>
+    main {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        gap: 1em;
+        margin: 1em;
+    }
+
+p {
+    color: black;
+}
+</style>
