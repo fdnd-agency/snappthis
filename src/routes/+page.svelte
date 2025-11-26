8a5ef4a20fb9097336b2aadf7589ad77c2a7ada4
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-11-26 09:18:49 +0100

    build: homepage
    a home page where you will return on
    
    #95

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,35 @@
 <script>
     import SnappMapPreview from '$lib/components/SnappMapPreview.svelte'
+    import Card from '$lib/components/Card.svelte'
+    import GalleryComponent from '$lib/components/GalleryComponent.svelte'
 
     let { data } = $props()
     const snappMaps = data.snappMaps
 </script>
+<main>
+<h1>Homepage</h1>
 
-<h1>SnappMap Overview</h1>
+<h2>Welcome back!</h2>
 
-{#each snappMaps as snappMap}
-    <SnappMapPreview {snappMap}></SnappMapPreview>
-{/each}
+<div class="last-snapps">
+    <Card text="your last placed snapps" />
+    <div class="gallery">
+        <GalleryComponent/>
+    </div>
+</div>
+</main>
 
 <style>
     h1 {
         font-family: 'Bariol', sans-serif;
         margin: 2rem auto;
         width: max-content;
+        flex-direction: row;
+    }
+
+    .last-snapps {
+        width: 30vw;
+        height: 30vh;
+        overflow: hidden;
     }
 </style>
