415ef237b8fa6123426d983418426aaee68fc81a
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-12-01 09:13:49 +0100

    build: build up of header
    What will be in the header?
    
    Also is it good to add dropdown menu to the header aswell needed for settings, add and layout-navigation
    
    #70

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,12 +1,28 @@
 <script>
     export let title = 'SnappThis'
 
+    import {GobackIcon, }
+
     import Goback from '$lib/components/icons/GobackIcon.svelte'
 </script>
 
 <header class="header">
 
+    <div class="left-icon">
+        <a>
+            <!-- goback icon -->
+        </a>
+    </div>
+
     <h1>{title}</h1>
+
+    <div class="right-icon">
+        <button>
+            <!-- add icon -->
+            <!-- layout-icon -->
+            <!-- setings icon -->
+        </button>
+    </div>
 
 </header>
 
