f397a25f5034b88c2845c5e4eaa38a3a073b6365
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-12-03 15:18:42 +0100

    Setup add button #134

---
+++ src/lib/components/AddButton.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    let { page } = $props()
+</script>
+
+<button>
+    <img src="src/lib/assets/CameraNeutral.svg" alt="Add Snapp">
+</button>
+
+<style>
+    button {
+        background-color: var(--accent-green);
+        height: 4em;
+        width: 4em;
+        border-radius: 50%;
+    }
+</style>
