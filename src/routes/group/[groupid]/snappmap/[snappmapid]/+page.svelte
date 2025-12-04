f4f7d5f61e442bfa5aa6b6f5ac15cb40e752dc20
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-12-04 10:52:20 +0100

    build: button
    made a button
    
    #138

--- src/routes/group/[groupid]/snappmap/[snappmapid]/+page.svelte
+++ src/routes/group/[groupid]/snappmap/[snappmapid]/+page.svelte
@@ -62,7 +62,7 @@
 <Header title={snapMap.name}></Header>
 <main>
 
-    <button bind:this={listItem}> GO </button>
+    <button class="list" bind:this={listItem}> GO </button>
 
 </main>
 
@@ -70,118 +70,23 @@
     main {
         overflow: scroll;
         margin-bottom: 5%;
-        display: grid;
-        grid-template-columns: 20% 80%;
         font-family: 'Bariol Bold';
+        display: flex;
+        justify-content: center;
+        margin-top: 2em;
 
         @media (max-width: 720px) {
             grid-template-columns: 1fr;
         }
     }
 
-    .layout-navigation {
-        background-color: var(--neutral-color-light);
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: flex-start;
-        height: 100%;
-        padding: 1em;
+    .list {
+        height: 4em;
+        width: 8em;
 
-        @media (max-width: 720px) {
-            flex-direction: row;
-            justify-content: center;
-            gap: 1em;
-        }
-
-        p {
-            font-family: 'Bariol bold';
-
-            @media (max-width: 720px) {
-                display: none;
-            }
-        }
-
-        label {
-            display: flex;
-            gap: 1em;
-        }
-
-        input {
-            display: none;
-        }
     }
 
-    .container {
-        display: grid;
-        gap: 2em;
-        top: 0;
-        list-style: none;
-        margin: 1em;
-    }
 
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 1em;
-        overflow: scroll;
-        height: 100vh;
-        gap: 1em;
-    }
 
-    img {
-        border-radius: 5%;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
 
-    .snaps-grid1 {
-        display: grid;
-        grid-template-columns: repeat(1, 1fr);
-    }
-
-    .snaps-grid2 {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-    }
-
-    .snaps-grid3 {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-    }
-
-    .snaps-grid4 {
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-    }
-
-    .snaps-grid5 {
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-    }
-
-    .list-container {
-        display: grid;
-        grid-template-rows: 50% 40% 10%;
-    }
-
-    .feedback {
-        display: flex;
-        justify-content: flex-end;
-    }
-
-    .list.visible {
-        display: grid;
-        grid-template-columns: 30% 70%;
-        padding: 1em;
-    }
-
-    .list-container {
-        display: none;
-    }
-
-    .list-container.visible {
-        display: block;
-    }
 </style>
