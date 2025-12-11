a8fa0e9794aab2d9b91d59277a720d994ac84225
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-12-11 18:20:51 +0100

    CSS variabelen responsive #142

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,83 +21,99 @@
 
 <style>
 main {
+    --border-radius: 50%;
+    --l: 12.5em;
+    --m: 5em;
+    --s: 3.5em;
+    --position-1: 0;
+    --position-2: 1.25em;
+    --position-3: 5em;
+  
     position: relative;
-    width: 30em;
-    height: 30em;
+    width: 15em;
+    height: 15em;
+  
+    @media ( width > 500px ) {
+        --l: 25em;
+        --m: 10em;
+        --s: 7em;
+        --position-1: 0;
+        --position-2: 2.5em;
+        --position-3: 10em;
+        width: 30em;
+        height: 30em;
+    }
 }
 
 div {
     position: absolute;
+    height: var(--m);
+    width: var(--m);
     background-color: pink;
     border: solid hotpink .5em;
-    height: 10em;
-    width: 10em;
-    border-radius: 5em;
-    animation: splat 1.5s;
+    border-radius: var(--border-radius);
     transition: easeOutBack;
     opacity: 0;
+    animation: splat 1.5s;
     animation-delay: calc( .5s + (.15s * sibling-index()));
 
     &:nth-of-type(1) {
-        top: 2.5em;
-        left: 2.5em;
-        height: 25em;
-        width: 25em;
-        border-radius: 12.5em;
+        top: var(--position-2);
+        left: var(--position-2);
+        height: var(--l);
+        width: var(--l);
+        border-radius: var(--border-radius);
         z-index: 1;
+        /* opacity: 1; */
         animation: splash 3s;
     }
 
     &:nth-of-type(2) {
-        top: 2.5em;
-        left: 2.5em;
+        top: var(--position-2);
+        left: var(--position-2);
     }
 
     &:nth-of-type(3) {
-        top: 2.5em;
-        right: 2.5em; 
+        top: var(--position-2);
+        right:var(--position-2); 
     }
 
     &:nth-of-type(4) {
-        bottom: 2.5em;
-        left: 2.5em;
+        bottom: var(--position-2);
+        left: var(--position-2);
     }
 
     &:nth-of-type(5) {
-        bottom: 2.5em;
-        right: 2.5em; 
+        bottom: var(--position-2);
+        right: var(--position-2); 
     }
 
     &:nth-of-type(6) {
-        bottom: 10em;
-        right: 0em; 
-        height: 7em;
-        width: 7em;
-        border-radius: 3.5em;
+        bottom: var(--position-3);
+        right: var(--position-1); 
+        height: var(--s);
+        width: var(--s);
     }
 
     &:nth-of-type(7) {
-        bottom: 0em;
-        right: 10em; 
-        height: 7em;
-        width: 7em;
-        border-radius: 3.5em;
+        bottom: var(--position-1);
+        right: var(--position-3); 
+        height: var(--s);
+        width: var(--s);
     }
 
     &:nth-of-type(8) {
-        top: 0em;
-        left: 12.5em; 
-        height: 7em;
-        width: 7em;
-        border-radius: 3.5em;
+        top: var(--position-1);
+        left: var(--position-3); 
+        height: var(--s);
+        width: var(--s);
     }
 
     &:nth-of-type(9) {
-        top: 10em;
-        left: 0em; 
-        height: 7em;
-        width: 7em;
-        border-radius: 3.5em;
+        top: var(--position-3);
+        left: var(--position-1); 
+        height: var(--s);
+        width: var(--s);
     }
 }
 
