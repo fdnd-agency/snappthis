bf8a5e4e15056474da41b2d0b33e19d3f989c728
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-12-11 14:09:20 +0100

    build: star-background
    Starry background
    
    #141

--- src/routes/group/[groupid]/snappmap/[snappmapid]/+page.svelte
+++ src/routes/group/[groupid]/snappmap/[snappmapid]/+page.svelte
@@ -25,12 +25,44 @@
 
     import Dropdown from '$lib/components/icons/Dropdown.svelte'
 
+    import { onMount } from 'svelte'
+
+    onMount(async () => {
+function random(min, max) {
+  return min + Math.random() * (max + 1 - min);
+}
+
+
+        const body = document.querySelector('body');
+        const canvasSize = body.offsetWidth * body.offsetHeight;
+        const starsFraction = canvasSize / 2000;
+
+for(let i = 0; i < starsFraction; i++) {
+  // Set up random elements
+  let xPos = random(0, 100);
+  let yPos = random(0, 100);
+  let alpha = random(0.5, 1);
+  let size = random(1, 2);
+  let colour = '#ffffff';
+    
+  // Add them to the body
+  const star = document.createElement('div');
+  star.style.position = 'relative';
+  star.style.left = xPos + '%';
+  star.style.top = yPos + '%';
+  star.style.opacity = alpha;
+  star.style.width = size + 'px';
+  star.style.height = size + 'px';
+  star.style.backgroundColor = colour;
+  document.body.appendChild(star);
+}
+    })
+
 </script>
 
 <svelte:head>
     <title>{snapMap.name}</title>
 </svelte:head>
-<div class="page">
 
 <header>
     <div class="title-block">
@@ -58,8 +90,6 @@
          <Dropdown />
     </div>
 </header>
-
-</div>
 
 <style>
     :root {
