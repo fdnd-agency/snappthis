6be47b54b10337dda1964956c4ceea06e2d344a4
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-06 10:11:27 +0100

    get data from DB
    to get the for each loop you need to load data from the database on the component.
    
    #71

--- src/lib/components/SnappMapDropdown.svelte
+++ src/lib/components/SnappMapDropdown.svelte
@@ -1,3 +1,22 @@
+<script>
+    import { onMount } from 'svelte'
+
+    onMount(async () => {
+    async function fetchSnappmap() {
+    const pictureResponse = await fetch(
+        'https://fdnd-agency.directus.app/items/snappthis_snapmap'
+    )
+
+    const snappmap = await pictureResponse.json()
+    return snappmap
+}
+    })
+
+console.log(fetchSnappmap)
+</script>
+<div class="active-card">
+    <Card text="{snappmap.title}" active="" />
+</div>
 <div class="dropdown">
     <!-- check if the snappmap is active -->
     <!-- for each loop -->
